6fa505c606366e1b2611e1eb66102c17c5344855
mrpapaj <kevin.nikolas123@gmail.com>
2019-02-28 22:13:51 +0100

    Remove real amount of currency

--- lab5/get_orderbook.py
+++ lab5/get_orderbook.py
@@ -9,14 +9,25 @@ def bitbayOrders(symbol):
 	upper_bid = bids[0]
 	upper_ask = asks[0]
 	bid_course = upper_bid[0]
-	bid_amount = upper_bid[1]
 	ask_course = upper_ask[0]
-	ask_amount = upper_ask[1]
-	bid_price = round(bid_course * bid_amount, 5)
-	ask_price = round(ask_course * ask_amount, 5)
 	# print("upper bid course:", bid_course, "| upper ask course:", ask_course)
-	return bid_price, bid_amount, ask_price, ask_amount
+	return bid_course, ask_course
+
+
+def binanceOrders(symbol):
+	response = requests.request("GET", "https://www.binance.com/api/v1/depth", params={"symbol": symbol})
+	data = response.json()
+	bids = data["bids"]
+	asks = data["asks"]
+	upper_bid = bids[0]
+	upper_ask = asks[0]
+	bid_course = float(upper_bid[0])
+	ask_course = float(upper_ask[0])
+	# print("upper bid course:", bid_course, "| upper ask course:", ask_course)
+	return bid_course, ask_course
 
 
 if __name__ == '__main__':
-	bitbayOrders("ethbtc")
+	symbol = "ETHBTC"
+	bitbayOrders(symbol)
+	binanceOrders(symbol)

f95ce139e56e6a2b4298336c9a3d02b25ef7fd0e
mrpapaj <kevin.nikolas123@gmail.com>
2019-03-01 18:42:54 +0100

    Add user balances with current amount of cryptocurrency

--- lab5/user_generator.py
+++ lab5/user_generator.py
@@ -4,6 +4,7 @@ import lab5.get_ticker
 import requests
 import random
 import time
+import json
 
 
 def userGenerator(number_of_users):
@@ -18,6 +19,9 @@ def userGenerator(number_of_users):
 		user["first_name"] = users[each_user]["name"]["first"]
 		user["last_name"] = users[each_user]["name"]["last"]
 		user["username"] = users[each_user]["login"]["username"]
+		user["wallet"] = {}
+		user["wallet"]["btc"] = 10
+		user["wallet"]["eth"] = 10
 		created_users.append(user)
 	# print(json.dumps(created_users, indent=2, sort_keys=False))
 	return created_users
@@ -25,15 +29,30 @@ def userGenerator(number_of_users):
 
 def pairUsers(users):
 	user_1, user_2 = random.sample(users, 2)
-	return user_1["first_name"], user_2["first_name"]
+	return user_1["username"], user_2["username"]
 
 
-def tradeCrypto(single_symbol, user_1, user_2, ask_course_bit, ask_course_bin):
-	crypto_amount = random.uniform(0.5, 1.9)
-	ask_price_bit = ask_course_bit * crypto_amount
-	ask_course_bin = ask_course_bin * crypto_amount
+def tradeCrypto(single_symbol, user_1, user_2, course_binance):
+	"""currently binance on fire"""
+	user_1_amount = random.uniform(0.5, 1.9)
+	user_2_amount = course_binance * user_1_amount
 	single_symbol = single_symbol.replace("BTC", "")
-	print(user_1, "exchanged", round(crypto_amount, 6), single_symbol, "with", user_2, "for", round(ask_course_bin, 6), "BTC")
+	print(user_1, "exchanged", round(user_1_amount, 6), single_symbol, "with", user_2, "for", round(user_2_amount, 6), "BTC")
+	return user_1, user_2, user_1_amount, user_2_amount
+
+
+def userBalances(users, user_1, user_2, user_1_amount, user_2_amount):
+	for user_id in range(0, len(users)):
+		user_match = users[user_id]["username"]
+		if user_1 == user_match:
+			users[user_id]["wallet"]["eth"] -= user_1_amount
+			users[user_id]["wallet"]["btc"] += user_2_amount
+		elif user_2 == user_match:
+			users[user_id]["wallet"]["eth"] += user_1_amount
+			users[user_id]["wallet"]["btc"] -= user_2_amount
+
+	print(json.dumps(users, indent=2, sort_keys=False))
+	return users
 
 
 def main():
@@ -46,13 +65,14 @@ def main():
 	symbol = get_string("cryptocurrency pair: ").upper()
 	print("-" * 10)
 	if symbol in linked_symbols:
-		users = userGenerator(number_of_users=100)
+		users = userGenerator(number_of_users=2)
 		transaction_limit = 10
 		while transaction_limit != 0:
 			user_1, user_2 = pairUsers(users)
-			bid_course_bit, ask_course_bit = bitbayOrders(symbol)
-			bid_course_bin, ask_course_bin = binanceOrders(symbol)
-			tradeCrypto(symbol, user_1, user_2, ask_course_bit, ask_course_bin)
+			# course_bitbay = bitbayOrders(symbol)
+			course_binance = binanceOrders(symbol)
+			user_1, user_2, user_1_amount, user_2_amount = tradeCrypto(symbol, user_1, user_2, course_binance)
+			userBalances(users, user_1, user_2, user_1_amount, user_2_amount)
 			transaction_limit -= 1
 			time.sleep(1.5)
 

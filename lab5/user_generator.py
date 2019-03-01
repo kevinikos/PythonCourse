4a0d6bba1d5cc67d1254d67063a434ddb29f7db9
mrpapaj <kevin.nikolas123@gmail.com>
2019-03-01 19:26:16 +0100

    Add ability to change type of second currency

--- lab5/user_generator.py
+++ lab5/user_generator.py
@@ -7,7 +7,7 @@ import time
 import json
 
 
-def userGenerator(number_of_users):
+def userGenerator(number_of_users, symbol):
 	created_users = []
 	response = requests.get("https://randomuser.me/api/?results=" + str(number_of_users))
 	data = response.json()
@@ -20,8 +20,8 @@ def userGenerator(number_of_users):
 		user["last_name"] = users[each_user]["name"]["last"]
 		user["username"] = users[each_user]["login"]["username"]
 		user["wallet"] = {}
-		user["wallet"]["btc"] = 10
-		user["wallet"]["eth"] = 10
+		user["wallet"]["BTC"] = 10
+		user["wallet"][symbol] = 10
 		created_users.append(user)
 	# print(json.dumps(created_users, indent=2, sort_keys=False))
 	return created_users
@@ -32,25 +32,24 @@ def pairUsers(users):
 	return user_1["username"], user_2["username"]
 
 
-def tradeCrypto(single_symbol, user_1, user_2, course_binance):
+def tradeCrypto(symbol, user_1, user_2, course_binance):
 	"""currently binance on fire"""
 	user_1_amount = random.uniform(0.5, 1.9)
 	user_2_amount = course_binance * user_1_amount
-	single_symbol = single_symbol.replace("BTC", "")
+	single_symbol = symbol.replace("BTC", "")
 	print(user_1, "exchanged", round(user_1_amount, 6), single_symbol, "with", user_2, "for", round(user_2_amount, 6), "BTC")
 	return user_1, user_2, user_1_amount, user_2_amount
 
 
-def userBalances(users, user_1, user_2, user_1_amount, user_2_amount):
+def userBalances(users, user_1, user_2, user_1_amount, user_2_amount, symbol):
 	for user_id in range(0, len(users)):
 		user_match = users[user_id]["username"]
 		if user_1 == user_match:
-			users[user_id]["wallet"]["eth"] -= user_1_amount
-			users[user_id]["wallet"]["btc"] += user_2_amount
+			users[user_id]["wallet"][symbol] -= user_1_amount
+			users[user_id]["wallet"]["BTC"] += user_2_amount
 		elif user_2 == user_match:
-			users[user_id]["wallet"]["eth"] += user_1_amount
-			users[user_id]["wallet"]["btc"] -= user_2_amount
-	return users
+			users[user_id]["wallet"][symbol] += user_1_amount
+			users[user_id]["wallet"]["BTC"] -= user_2_amount
 
 
 def main():
@@ -62,14 +61,15 @@ def main():
 	print("-" * 10)
 	symbol = get_string("cryptocurrency pair: ").upper()
 	print("-" * 10)
+	part_of_symbol = symbol.replace("BTC", "")
 	if symbol in linked_symbols:
-		users = userGenerator(number_of_users=5)
+		users = userGenerator(number_of_users=5, symbol=part_of_symbol)
 		transaction_limit = 2
 		while transaction_limit != 0:
 			user_1, user_2 = pairUsers(users)
 			course_binance = binanceOrders(symbol)
 			user_1, user_2, user_1_amount, user_2_amount = tradeCrypto(symbol, user_1, user_2, course_binance)
-			userBalances(users, user_1, user_2, user_1_amount, user_2_amount)
+			userBalances(users, user_1, user_2, user_1_amount, user_2_amount, part_of_symbol)
 			transaction_limit -= 1
 			time.sleep(1.5)
 		print("-" * 10)

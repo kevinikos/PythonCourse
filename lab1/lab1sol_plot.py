ff7cf0931d0d65f2ef3fac2385ef0c81473d056d
mrpapaj <kevin.nikolas123@gmail.com>
2019-03-12 01:14:25 +0100

    Add initial value

--- lab1/lab1sol_plot.py
+++ lab1/lab1sol_plot.py
@@ -4,12 +4,10 @@ from matplotlib.pyplot import *
 import random
 
 
-def drawChart(x1, x2):
-	t = np.arange(x1, x2, 0.01)
+def drawChart(x2):
+	t = np.arange(0.0, x2, 0.01)
 	s = np.cos(2 * np.pi * t)
 	plot(t, s, "y", "*")
 	show()
 
-
-for i in range(0, 5):
-	drawChart(x1=0.0, x2=random.uniform(0.5, 10))
+drawChart(x2=10.0)
--- lab2/lab2sol_plot.py
+++ lab2/lab2sol_plot.py
@@ -6,20 +6,16 @@ import numpy as np
 
 print("""#5 Look at lab2-plot.py and create your own script which takes a number as an input and plots the same 3D wave but with different characteristics "
 it's totally up to your imagination how do you want to amend the figure according to the input number (1p)""")
-
-value = get_int("value: ") # suggested range 5-10
-limit = 4
-start_value = 0
-for i in range(0,limit):
-	part = value / limit
-	print("part:", part)
-	start_value += part
-	print(start_value)
-	x_knots = np.linspace(-start_value, start_value, 200)
-	y_knots = np.linspace(-start_value, start_value, 200)
+def drawChart(value):
+	# value = get_int("value: ") # suggested range 5-10
+	x_knots = np.linspace(-value * np.pi, value * np.pi, 200)
+	y_knots = np.linspace(-value * np.pi, value * np.pi, 200)
 	X, Y = np.meshgrid(x_knots, y_knots)
 	R = np.sqrt(X ** 2 + Y ** 2)
+	# Z = np.cos(R) ** 2 * np.exp(-0.1 * R)
 	Z = np.sin(R)
 	ax = Axes3D(plt.figure(figsize=(8, 5)))
 	ax.plot_surface(X, Y, Z, cmap='viridis', edgecolor='none')
 	plt.show()
+
+drawChart(value=3)
